9cb631c0461d75207eb80b900c88a3166823903a
Zach Pflederer <zach@giveforward.com>
2017-06-14 22:23:45 -0500

    update: temp bg image, model link

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div
+        className="App"
+        style={{ backgroundImage: 'url(https://image.shutterstock.com/z/stock-photo-cleaning-concept-young-woman-cleaning-office-547592146.jpg)', height: '100vh' }}>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload. We'll model this site after Cleanify,
+          it has a simple interface, and makes it easy for users to request a cleaning. Later, we'll try to
+          work on SEO.
         </p>
+        <a href="https://www.cleanify.com/">Cleanify</a>
       </div>
     );
   }
