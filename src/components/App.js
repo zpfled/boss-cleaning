526af6b1907f52ee7b5a882ac891952406e7b591
Zach Pflederer <zach@giveforward.com>
2017-07-02 21:01:47 -0500

    add: services section, pencil in reviews section and footer

--- src/components/App.js
+++ src/components/App.js
@@ -8,21 +8,44 @@ import RaisedButton from 'material-ui/RaisedButton';
 import Paper from 'material-ui/Paper';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { StickyContainer, Sticky } from 'react-sticky';
+import { LinkButton, LoadingWrapper } from 'fastmodel-layout-components';
+import TableTop from 'tabletop';
+import Promise from 'bluebird';
 
+const SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1QpujZJ378YYALyVtSPPSvZG4P1ZP8JljC7Fcmm0sleo/pub?output=csv';
 // This replaces the textColor value on the palette
 // and then update the keys for each component that depends on it.
 // More on Colors: http://www.material-ui.com/#/customization/colors
+const PRIMARY_COLOR = '#1e88e5';
+const HEADER_COLOR = { color: PRIMARY_COLOR };
 const bossTheme = getMuiTheme({
   palette: {
-    primary1Color: '#4267b2',
-    textColor: '#4267b2',
+    primary1Color: PRIMARY_COLOR,
+    textColor: '#444',
   },
   appBar: {
     height: 50,
   },
 });
 
+
 class App extends Component {
+
+  state = {
+    services: [],
+    loadingServices: false,
+  };
+
+  componentWillMount() {
+    this.setState({ loadingServices: true }, () => {
+      TableTop.init({
+        key: SPREADSHEET_URL,
+        callback: (services) => this.setState({ services, loadingServices: false }),
+        simpleSheet: true,
+      });
+    });
+  }
+
   render() {
     return (
       <MuiThemeProvider muiTheme={bossTheme}>
@@ -46,7 +69,7 @@ class App extends Component {
                   </div>
                   <span>
                   <RaisedButton
-                    onTouchTap={() => console.log('link to section below with quote wizard')}
+                    onTouchTap={() => alert('link to section below with quote wizard')}
                     label="Get a quote"
                     containerElement="button"
                     primary
@@ -72,7 +95,7 @@ class App extends Component {
                 <div style={{ position: 'relative', top: 130, width: '100vw', textAlign: 'center' }}>
                   <RaisedButton
                     style={{ margin: 'auto', minWidth: '80vw', width: '80vw' }}
-                    onTouchTap={() => console.log('link to section below with quote wizard')}
+                    onTouchTap={() => alert('link to section below with quote wizard')}
                     label="Schedule a cleaning today"
                     containerElement="button"
                     primary
@@ -85,11 +108,11 @@ class App extends Component {
               <div className="display-none-small-down flex-grow-1">
                 <div
                   className="display-flex align-items-center flex-direction-column justify-content-center height-500">
-                  <h1 style={{ color: '#4267b2' }}>Schedule a cleaning today.</h1>
+                  <h1 style={HEADER_COLOR}>Schedule a cleaning today.</h1>
                   <RaisedButton
                     style={{ height: 100, width: 300 }}
                     labelStyle={{ fontSize: 24 }}
-                    onTouchTap={() => console.log('link to section below with quote wizard')}
+                    onTouchTap={() => alert(`This will lead to requesting a quote for a Cleaning`)}
                     label="Get a quote"
                     containerElement="button"
                     primary
@@ -98,18 +121,68 @@ class App extends Component {
               </div>
               <div className="flex-grow-1"></div>
             </section>
-            <section>
-              <h2>Services</h2>
+
+            <LoadingWrapper className="mg-top-xl" loading={!!this.state.loadingServices}>
+              <section className="max-width">
+                <h2 style={{ ...HEADER_COLOR, textAlign: 'center' }}>
+                  How does it work?
+                </h2>
+                <p className="xl-font" style={{ textAlign: 'center', lineHeight: 1.5 }}>
+                  Here at Boss Cleaning, our mission is to free up your time by handling the jobs that keep you from
+                  doing what you really want to be doing. Our reliable, meticulous crews will keep your home or office
+                  looking like new while you kick back and relax. We offer a variety of services to revitalize your
+                  home or office and help you take back your free time. Just click on any of the services listed below
+                  to get started!
+                </p>
+                <ul className="display-flex flex-wrap-wrap pd-left-none justify-content-center">
+                  {
+                    this.state.services.map(({ name, cost, basis, description }) => {
+                      return (
+                        <li style={{ listStyle: 'none' }} className="mg-sm">
+                          <RaisedButton
+                            style={{ height: 'auto' }}
+                            onTouchTap={() => alert(`This will lead to requesting a quote for a ${name}`)}>
+                            <Paper className="pd-md max-width-400 min-width-350">
+                              <h3 style={HEADER_COLOR} className="mg-top-none mg-bottom-none">
+                                {name}
+                              </h3>
+                              <h4>{cost ? `$${cost}` : 'Price Varies'}{basis == 'hourly'&& ' an hour'}</h4>
+                              <p>
+                                {description}
+                              </p>
+                            </Paper>
+                          </RaisedButton>
+                        </li>
+                      )
+                    })
+                  }
+                </ul>
+              </section>
+            </LoadingWrapper>
+
+            <section style={{ textAlign: 'center' }} className="pd-lg">
+              <Paper>
+                <h2 className="pd-lg">reviews go here</h2>
+              </Paper>
             </section>
-            <section>
-              <h2>Request quote wizard</h2>
+
+            <section style={{ textAlign: 'center' }} className="pd-lg">
+              <RaisedButton
+                style={{ height: 100, width: 300 }}
+                labelStyle={{ fontSize: 24 }}
+                onTouchTap={() => alert(`This will lead to requesting a quote for a Cleaning`)}
+                label="Get a quote"
+                containerElement="button"
+                primary
+              />
             </section>
-            <section>
-              <h2>reviews</h2>
-            </section>
-            <section>
-              <h2>footer/social</h2>
-            </section>
+
+            <footer className="bg-color-secondary color-white display-flex justify-content-space-between pd-lg">
+              <ul className="max-width">
+                <li>Contact Info</li>
+                <li>Social Media Info</li>
+              </ul>
+            </footer>
           </main>
         </StickyContainer>
       </MuiThemeProvider>
